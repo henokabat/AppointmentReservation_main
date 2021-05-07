57a773922f6903513458b42e7af4a194fcd290ff
Henok Abatihun <33490057+henokabat@users.noreply.github.com>
2021-05-07 14:56:05 -0500

    Initial commit

---
+++ src/main/java/com/miu/edu/appointmentreservation/AppointmentReservationApplication.java
@@ -0,0 +1,13 @@
+package com.miu.edu.appointmentreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentReservationApplication.class, args);
+    }
+
+}
